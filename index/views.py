ab3315555c3ee14ed37870a435b65a1e7dd6080d
inyeong <kiy7833@gmail.com>
2021-08-01 19:01:41 +0900

    Update views.py & Add index.html and style.css

--- index/views.py
+++ index/views.py
@@ -3,69 +3,80 @@ from django.http import HttpResponse, JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
 from library.df_response_lib import *
-import json, requests
 
-def get_answer(text):
-    data_send = { 
-        "queryInput":{
-        "text":{
-            "text": text,
-            "languageCode" :"ko"
-        }
-        }
-    }
-
-    MY_AUTH_TOKEN="Bearer ya29.a0ARrdaM_MeKOc2fn0dtDuWt9eMFsc6UYIcNGwS1mZnLBN41fqKyQHYw7Y9MAzYEoJMIWXdQHFevey8qy79Qnhy9FvLI3SQjRdFxRDb4XK3TZyM7nxtq_aaBE7Twgxu_vZQJ05IATyxHOylTxc2qbev-arN7lZxPkyf0GDMWUmoDBHQu3nJ_zCF_e8idB-d584OgjkRXCRvRslpRcc-TZKfA8Qa5u9kmr0drEDhXftuezu-ws"
-    
-    data_header = {
-        'Authorization': MY_AUTH_TOKEN,
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-
-    dialogflow_url = 'https://dialogflow.clients6.google.com/v2beta1/projects/emochatbot-aupx/locations/global/agent/sessions/704c9faa-a2ba-4f3b-e9c6-a394311753f2:detectIntent'
-    res = requests.post(dialogflow_url, data=json.dumps(data_send), headers=data_header)
-
-    if res.status_code != requests.codes.ok:
-        return '오류가 발생했습니다.'
-    else:
-        data_receive = res.json()
-        answer = data_receive['queryResult']['fulfillmentText']
-        print(f"answer : {answer}")
-        return JsonResponse(str(answer), safe=False)
+from google.cloud import dialogflow
+from google.protobuf.json_format import MessageToJson
 
 
 def index(request):
     return render(request, 'index.html')
 
-# @csrf_exempt
-# def webhook(request):
-#     print('Connected webhook')
-#     # build a request object
-#     req = json.loads(request.body)
-#     print(f"req: {req}")
-#     # get action from json
-#     action = req.get('queryResult').get('action')
-#     # get fulfillment from json
-#     fulfillmentText = req.get('queryResult').get('fulfillmentText')
-#     fulfillmentMessages = req.get('queryResult').get('fulfillmentMessages')
-#     # return a fulfillment message
-#     ff_response = fulfillment_response()
-#     ff_text = ff_response.fulfillment_text(fulfillmentText)
-#     ff_messages = ff_response.fulfillment_messages(fulfillmentMessages)
-#     reply = ff_response.main_response(ff_text, ff_messages)
-#     print('reply: {0}'.format(reply))
-#     # return response
-#     return JsonResponse(reply, safe=False)
 
 @csrf_exempt
-def webhook(request):
-    print(f"request: {request}")
-    content = request.GET.get('content')
-    print(f"content: {content}")
-    result =  get_answer(content)
-    print(f"re : {result}")
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
 
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    
+    session_client = dialogflow.SessionsClient()
 
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
 
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    # print("Query result: {}".format(response.query_result))
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    fulfillmentMessages = response.query_result.fulfillment_messages
+    fulfillmentText = response.query_result.fulfillment_text
+    # print("fulfillmentMessages: {}\n".format(fulfillmentMessages))
+
+    if fulfillmentText:
+        # 인텐트에 데한 리스폰스
+        msg = ""
+        
+        for message in fulfillmentMessages:
+            # print(f"message: {message.text.text[0]}")
+            msg += str(message.text.text[0]) + " "
+        answer = msg
+        # print(f"answer: {answer}")
+    else:
+        # '그래' 답을 했을 경우
+        msg = ""
+        
+        # print(f"msg1: {fulfillmentMessages}")
+        # msg += str(fulfillmentMessages) + " "
+
+        # json_string = MessageToJson(fulfillmentMessages)
+        
+        for message in fulfillmentMessages:
+            print(f"msg3: {type(message)}")
+            msg += str(type(message)) + " "
+        msg = response
+        answer = msg
+   
+    return answer
+
+@csrf_exempt
+def response(request):
+    project_id  = 'emochatbot-aupx'
+    session_id = '704c9faa-a2ba-4f3b-e9c6-a394311753f2'
+    texts = request.GET.get('msg')
+    language_code = 'ko'
+    return HttpResponse(detect_intent_texts(project_id, session_id, texts, language_code))
 
 
