a34ae39f04108bfc35f175def077493aea00fa38
shinjuno <shinjuno123@naver.com>
2021-08-03 11:06:55 -0700

    new

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = [
     'awdjaw.herokuapp.com',
+    '127.0.0.1'
 ]
 
 
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- index/views.py
+++ index/views.py
@@ -3,31 +3,61 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 #import df_library
 from library.df_response_lib import *
-import json
+from google.api_core.exceptions import InvalidArgument
+import os
+import ast
+import dialogflow
 
-def index(request):
-    return render(request, 'index.html')
+class webAPI:
+    def __init__(self,saying):
+        self.DIALOGFLOW_PROJECT_ID = 'chatbot-testo-dqoh'
+        self.DIALOGFLOW_LANGUAGE_CODE = 'ko-KR'
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./static/chatbot-testo-dqoh-0dd0110b9946.json"
+        self.SESSION_ID = 'shinjuno123@gmail.com'
+        self.text_to_be_analyzed = saying
+
+    def SendQuestionsAndGetAnswerFromChatBot(self):
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(self.DIALOGFLOW_PROJECT_ID, self.SESSION_ID)
+        text_input = dialogflow.types.TextInput(text=self.text_to_be_analyzed, language_code=self.DIALOGFLOW_LANGUAGE_CODE)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        try:    
+            response = session_client.detect_intent(session=session, query_input=query_input)
+        except InvalidArgument:
+            raise
+        # print("Query text:", response.query_result.query_text)
+        # print("Detected intent:", response.query_result.intent.display_name)
+        # print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+        print("Fulfillment text:", response.query_result.fulfillment_text)
+        return response.query_result.fulfillment_text
+
+    
+
+
+
+
+
 
 @csrf_exempt
-def webhook(request):
-    print('Connected webhook')
-    # build a request object
-    req = json.loads(request.body)
-    # get action from json
-    action = req.get('queryResult').get('action')
-    # get fulfillment from json
-    fulfillmentText = req.get('queryResult').get('fulfillmentText')
-    fulfillmentMessages = req.get('queryResult').get('fulfillmentMessages')
-    # return a fulfillment message
-    ff_response = fulfillment_response()
-    ff_text = ff_response.fulfillment_text(fulfillmentText)
-    ff_messages = ff_response.fulfillment_messages(fulfillmentMessages)
-    reply = ff_response.main_response(ff_text, ff_messages)
-    # print('reply: {0}'.format(reply))
-    print(reply)
-    return JsonResponse(reply, safe=False)
+def index(request):
+    if(request.method == "POST"):
+        # print(type(ast.literal_eval(request.body.decode('UTF-8'))))
+        saying = ast.literal_eval(request.body.decode('UTF-8'))
+        response_obj = {}
+        response_obj['res'] = webAPI(saying['saying']).SendQuestionsAndGetAnswerFromChatBot()
+        return JsonResponse(response_obj)
+    return render(request, 'index.html')
 
 
+
+
+
+'''
+      intent="WELCOME"
+      chat-title="EmoChatbot"
+      agent-id="828613f6-4761-4d79-9ba9-920f4d5f2c6e"
+      language-code="ko"
+'''
 '''
 @csrf_exempt
 def webhook(request):
