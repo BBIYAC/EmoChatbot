4fc0979f36c0cf705db4c71344cd77a1f476d15a
junhoshin <shinjuno123@naver.com>
2021-09-01 15:36:21 +0900

    modify login and signup pages

--- static/Login/login/login.js
+++
@@ -1,8 +0,0 @@
-window.scrollTo(0,0)
-var container = document.getElementById('big-container')
-container.style.height = window.innerHeight + "px";
-
-window.addEventListener('resize',(event)=>{
-    var container = document.getElementById('big-container')
-    container.style.height = window.innerHeight + "px";
-})
